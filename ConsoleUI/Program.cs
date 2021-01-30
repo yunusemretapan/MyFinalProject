ec4e7af2b0957836d786947bf50700bcc90a7026
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-01-31 00:05:28 +0300

    MyFinalProject

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,17 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
         }
     }
 }
