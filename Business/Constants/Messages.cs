3ff421552bfc4b98b2e3b8b04ed9948c4893ef9d
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-02-11 00:02:39 +0300

    MyFinalProject

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -5,7 +5,7 @@
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        public static string ProductsListed = "Ürün listelendi";
-        public static string MaintenanceTime = "Bakım zamanı";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string MaintenanceTime = "Sistem bakımda";
     }
 }
