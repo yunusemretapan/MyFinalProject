08a9ea4363ba81fa8a7049b7572504bde277f580
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-02-10 23:50:57 +0300

    MyFinalProject

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,11 @@
+﻿namespace Business.Constants
+{
+    //static olduğu için class new'lenemez
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string ProductsListed = "Ürün listelendi";
+        public static string MaintenanceTime = "Bakım zamanı";
+    }
+}
