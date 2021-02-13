dbb7ea79473527ff8bb61b3f9b7a280de2e70d1d
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-02-13 23:55:43 +0300

    MyFinalProject

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -30,7 +30,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
