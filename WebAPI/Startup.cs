7c2f34ceb291eca85e787274dd2420c20e970b08
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-02-18 00:04:13 +0300

    MyFinalProject

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -4,16 +4,9 @@ using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI
 {
@@ -33,9 +26,9 @@ namespace WebAPI
             //AOP 
             services.AddControllers();
             //IProductService isteðine (new ProductManager) verilmiþ oluyor
-            services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductService, ProductManager>();
             //IProductDal isteðine (new EfProductDal) verilmiþ oluyor
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
